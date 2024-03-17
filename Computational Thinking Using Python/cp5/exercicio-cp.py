45d117c8b3d6ab92088c59d467ce802b64026e38
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-03-17 19:50:57 -0300

    Update exercicio-cp.py

--- Computational Thinking Using Python/cp5/exercicio-cp.py
+++ Computational Thinking Using Python/cp5/exercicio-cp.py
@@ -17,7 +17,7 @@ class Animal:
         pass
 
     def __str__(self):
-        return (f'....: ANIMAL #{self.id} :.....\nNome...: {self.nome}\nIdade..: {self.idade}\nCor....: {self.cor}\n'
+        return (f'\n....: ANIMAL #{self.id} :.....\nNome...: {self.nome}\nIdade..: {self.idade}\nCor....: {self.cor}\n'
                 f'Sexo...: {self.sexo}\nStatus.: {"Vivo" if self.status else "Morto"}\n')
 
 
@@ -92,25 +92,6 @@ class Gato(Mamifero):
             return self.nome + 'já morreu.'
 
 
-# ----- PEIXES -----
-class Peixe(Animal):
-    def __init__(self, nome, idade, cor, sexo, status):
-        super().__init__(nome, idade, cor, sexo, status)
-        
-
-    def amamentar(self):
-        if self.sexo == 'F':
-            if self.status:
-                return 'Amamentando bebêzinho!'
-            else:
-                return 'Ela já morreu :('
-        else:
-            return 'Machos não amamentam! hihi'
-
-    def __str__(self):
-        return super().__str__() + f'Tipo...: {self.__class__.__name__}\n'
-
-
 class Leao(Mamifero):
     def __init__(self, nome, idade, cor, sexo, status, tipo_do_pelo):
         super().__init__(nome, idade, cor, sexo, status, tipo_do_pelo)
@@ -128,15 +109,117 @@ class Leao(Mamifero):
             return self.nome + 'está morto'
 
 
+class Golfinho(Mamifero):
+    def __init__(self, nome, idade, cor, sexo, status, tipo_do_pelo):
+        super().__init__(nome, idade, cor, sexo, status, tipo_do_pelo)
+
+    def emitir_som(self):
+        if self.status:
+            return 'AAAAAN AAAAN AAAAAAAN'
+        else:
+            return self.nome + 'está morto'
+
+    def movimentar(self):
+        if self.status:
+            return self.nome + ' está pulando no Sea Park!'
+        else:
+            return self.nome + 'está morto'
+
+
+# ----- PEIXES -----
+class Peixe(Animal):
+    def __init__(self, nome, idade, cor, sexo, status, tipo_agua):
+        super().__init__(nome, idade, cor, sexo, status)
+        self.tipo_agua = tipo_agua
+
+    def movimentar(self):
+        if self.status:
+            return self.nome + ' está nadando!'
+        else:
+            return self.nome + ' já morreu :('
+
+    def emitir_som(self):
+        if self.status:
+            return 'SPLAAASH SPLAAAAAH'
+        else:
+            return self.nome + 'já morreu.'
+
+    def __str__(self):
+        return super().__str__() + f'Tipo...: {self.__class__.__name__}\nLocal:...: {self.tipo_agua}\n'
+
+
+class Tubarao(Peixe):
+    def __init__(self, nome, idade, cor, sexo, status, tipo_agua):
+        super().__init__(nome, idade, cor, sexo, status, tipo_agua)
+
+    def movimentar(self):
+        if self.status:
+            return self.nome + ' está atacando pessoas na praia!'
+        else:
+            return 'Ela já morreu :('
+
+
+class Piranha(Peixe):
+    def __init__(self, nome, idade, cor, sexo, status, tipo_agua):
+        super().__init__(nome, idade, cor, sexo, status, tipo_agua)
+
+    def movimentar(self):
+        if self.status:
+            return self.nome + ' está atacando seu marido!'
+        else:
+            return 'Ela já morreu, pois mexeu com homem casado :('
+
+    def emitir_som(self):
+        if self.status:
+            return 'OII VIDAA, ME PAGA UM COMBO???? ;)'
+        else:
+            return self.nome + 'já morreu.'
+
+
+class Baiacu(Peixe):
+    def __init__(self, nome, idade, cor, sexo, status, tipo_agua, inflado):
+        super().__init__(nome, idade, cor, sexo, status, tipo_agua)
+        self.inflado = inflado
+
+    def inflar(self):
+        if self.inflado:
+            return self.nome + ' já está inflado'
+        else:
+            return self.nome + ' INFLOUUUUUUU!'
+
+
+# 
+class Peixe(Animal):
+    def __init__(self, nome, idade, cor, sexo, status, tipo_agua):
+        super().__init__(nome, idade, cor, sexo, status)
+        self.tipo_agua = tipo_agua
+
+    def movimentar(self):
+        if self.status:
+            return self.nome + ' está nadando!'
+        else:
+            return self.nome + ' já morreu :('
+
+    def emitir_som(self):
+        if self.status:
+            return 'SPLAAASH SPLAAAAAH'
+        else:
+            return self.nome + 'já morreu.'
+
+    def __str__(self):
+        return super().__str__() + f'Tipo...: {self.__class__.__name__}\nLocal:...: {self.tipo_agua}\n'
+
+
+
 
 # Instanciando animais
 leao = Leao('Simba', 3, 'Begezinho', 'M', True, 'Longo')
 gato = Gato('Bernadete', 1, 'Rajadinho', 'F', True, 'Curto', 'SRD')
 macaco = Macaco('Donkey', 6, 'Marrom', 'M', False, 'Curto')
-
-#macaco = Macaco('Donkey', 6, 'Marrom', 'M', False, 'Curto')
-#macaco = Macaco('Donkey', 6, 'Marrom', 'M', False, 'Curto')
-#macaco = Macaco('Donkey', 6, 'Marrom', 'M', False, 'Curto')
+golfinho = Golfinho('Delphi', 4, 'Cinzinha', 'M', True, 'Não têm')
+tubarao = Tubarao('Cleitinho', 10, 'Cinza', 'M', True, 'Água Salgada')
+piranha = Piranha('LeidyEllin', 4, 'Verde Brilhante', 'F', True, 'Água Doce')
+baiacu = Baiacu('Pipoka', 8, 'Marrom com Bege', 'M', True, 'Água Doce', False)
 
 # Animal 1
 print(leao)
@@ -155,3 +238,25 @@ print(macaco)
 print(macaco.emitir_som())
 print(macaco.amamentar())
 print(macaco.movimentar())
+
+# Animal 4
+print(golfinho)
+print(golfinho.emitir_som())
+print(golfinho.amamentar())
+print(golfinho.movimentar())
+
+# Animal 5
+print(tubarao)
+print(tubarao.emitir_som())
+print(tubarao.movimentar())
+
+# Animal 6
+print(piranha)
+print(piranha.movimentar())
+print(piranha.emitir_som())
+
+# Animal 7
+print(baiacu)
+print(baiacu.movimentar())
+print(baiacu.emitir_som())
+print(baiacu.inflar())
