a82f05b54776bd7ba83b856fa6b1c997e4aa6915
Claudio Silva Bispo <110735259+Claudio-Silva-Bispo@users.noreply.github.com>
2025-03-10 10:03:50 -0700

    Add files via upload

---
+++ Advanced_Business _With_.NET/sprint-3/Infrastructure/Data/MongoDbContext.cs
@@ -0,0 +1,25 @@
+using MongoDB.Driver;
+using Project.Models;
+
+namespace Project.Infrastructure
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("MongoDbConnection");
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase("Project");
+        }
+
+        public IMongoCollection<Usuario> Usuarios => _database.GetCollection<Usuario>("t_usuario");
+        public IMongoCollection<Login> Login => _database.GetCollection<Login>("t_login");
+        public IMongoCollection<Endereco> Endereco => _database.GetCollection<Endereco>("t_endereco");
+        public IMongoCollection<DiasPreferencia> DiasPreferencia => _database.GetCollection<DiasPreferencia>("t_dias_preferencia");
+        public IMongoCollection<Turno> Turno => _database.GetCollection<Turno>("t_turno");
+        public IMongoCollection<Horarios> Horarios => _database.GetCollection<Horarios>("t_horario_preferencia");
+        public IMongoCollection<Clinica> Clinica => _database.GetCollection<Clinica>("t_clinica");
+    }
+}
