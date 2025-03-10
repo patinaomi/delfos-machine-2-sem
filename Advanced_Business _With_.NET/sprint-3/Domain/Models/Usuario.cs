cbd3ad1d09970dbd7bd5eabcae1b3165dbccd3f3
Claudio Silva Bispo <110735259+Claudio-Silva-Bispo@users.noreply.github.com>
2025-03-10 16:32:11 -0700

    Add files via upload

--- Advanced_Business _With_.NET/sprint-3/Domain/Models/Usuario.cs
+++ Advanced_Business _With_.NET/sprint-3/Domain/Models/Usuario.cs
@@ -14,5 +14,6 @@ namespace Project.Models;
         public string? Telefone { get; set; }
         public string? Email { get; set; }
         public string? Senha { get; set; }
+        public string Perfil { get; set; } = "Comum";
 
     }
