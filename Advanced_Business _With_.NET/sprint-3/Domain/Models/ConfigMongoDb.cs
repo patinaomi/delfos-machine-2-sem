5777a11daf76e0f59203a204a499a24f5bd7a36a
Claudio Silva Bispo <110735259+Claudio-Silva-Bispo@users.noreply.github.com>
2025-03-10 09:55:46 -0700

    FEAT - Criação das Models

---
+++ Advanced_Business _With_.NET/sprint-3/Domain/Models/ConfigMongoDb.cs
@@ -0,0 +1,15 @@
+    namespace Project.Domain
+    {
+        public class ConfigMongoDb
+        {
+            public string ConnectionString { get; set; } = null!;
+            public string DatabaseName { get; set; } = null!;
+            public string UsuarioCollectionName { get; set; } = null!;
+            public string LoginCollectionName { get; set; } = null!;
+            public string EnderecoCollectionName { get; set; } = null!;
+            public string DiasPreferenciaCollectionName { get; set; } = null!;
+            public string TurnoCollectionName { get; set; } = null!;
+            public string HorariosCollectionName { get; set; } = null!;
+            public string ClinicaCollectionName { get; set; } = null!;
+        }
+    }
